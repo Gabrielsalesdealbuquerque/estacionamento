e6df208f1eabae8a69fc78813410510bfebddf1c
Gabriel Albuquerque <114250471+Gabrielsalesdealbuquerque@users.noreply.github.com>
2023-05-03 14:43:14 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "carros";
+$servidor = "dbservergs.mysql.database.azure.com";
+$usuario = "admi1";
+$senha = "$#nai@115";
+$dbname = "petshop";
 
 // criar a conexão - string de conexão
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
